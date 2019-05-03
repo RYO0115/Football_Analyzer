054d757db990481b49a2b0b968f52aa99d979f80
RYO0115 <pastmemories.meiji@gmail.com>
2019-05-03 20:27:13 +0900

    zorderを指定して描画の順番を設定

--- src/drawFootballCourt.py
+++ src/drawFootballCourt.py
@@ -24,44 +24,43 @@ class DRAW_COURT():
 
     def DrawCourtCircle(self):
         #センターサークル
-        circle = plt.Circle( xy=(0,0), radius=9.150, ec='w', fc='w', fill=False)
+        circle = plt.Circle( xy=(0,0), radius=9.150, ec='w', fc='w', fill=False, zorder=1)
         self.ax.add_patch(circle)
 
         #ペナルティスポット
-        circle = plt.Circle( xy=( 61.5, 0), radius=0.5, ec="k", fc="k", fill=True)
+        circle = plt.Circle( xy=( 61.5, 0), radius=0.5, ec="k", fc="k", fill=True, zorder=1)
         self.ax.add_patch(circle)
-        circle = plt.Circle( xy=(-61.5, 0), radius=0.5, ec="k", fc="k", fill=True)
+        circle = plt.Circle( xy=(-61.5, 0), radius=0.5, ec="k", fc="k", fill=True, zorder=1)
         self.ax.add_patch(circle)
 
     def DrawPlayerCircle(self, x, y, color):
-        circle = plt.Circle( xy=(x,y), radius=2, ec="w", fc=color, fill=True)
+        circle = plt.Circle( xy=(x,y), radius=2, ec="w", fc=color, fill=True, zorder=2)
         self.ax.add_patch(circle)
 
 
     def DrawCourtLine(self):
         #センターライン
-        self.DrawLine( 0, 0,-COURT_SIZE[1], COURT_SIZE[1])
+        self.DrawLine( 0, 0,-COURT_SIZE[1], COURT_SIZE[1], zo=1)
 
         #ゴールエリア
-        self.DrawLine( 67.0, 67.0,-9.16, 9.16)
-        self.DrawLine(-67.0,-67.0,-9.16, 9.16)
-        self.DrawLine( 67.0, COURT_SIZE[0], 9.16, 9.16)
-        self.DrawLine( 67.0, COURT_SIZE[0],-9.16,-9.16)
-        self.DrawLine(-67.0,-COURT_SIZE[0], 9.16, 9.16)
-        self.DrawLine(-67.0,-COURT_SIZE[0],-9.16,-9.16)
+        self.DrawLine( 67.0, 67.0,-9.16, 9.16, zo=1)
+        self.DrawLine(-67.0,-67.0,-9.16, 9.16, zo=1)
+        self.DrawLine( 67.0, COURT_SIZE[0], 9.16, 9.16, zo=1)
+        self.DrawLine( 67.0, COURT_SIZE[0],-9.16,-9.16, zo=1)
+        self.DrawLine(-67.0,-COURT_SIZE[0], 9.16, 9.16, zo=1)
+        self.DrawLine(-67.0,-COURT_SIZE[0],-9.16,-9.16, zo=1)
 
         #ペナルティエリア
-        self.DrawLine( 56.0, 56.0,-20.16, 20.16)
-        self.DrawLine(-56.0,-56.0,-20.16, 20.16)
-        self.DrawLine( 56.0, COURT_SIZE[0], 20.16, 20.16)
-        self.DrawLine( 56.0, COURT_SIZE[0],-20.16,-20.16)
-        self.DrawLine(-56.0,-COURT_SIZE[0], 20.16, 20.16)
-        self.DrawLine(-56.0,-COURT_SIZE[0],-20.16,-20.16)
+        self.DrawLine( 56.0, 56.0,-20.16, 20.16, zo=1)
+        self.DrawLine(-56.0,-56.0,-20.16, 20.16, zo=1)
+        self.DrawLine( 56.0, COURT_SIZE[0], 20.16, 20.16, zo=1)
+        self.DrawLine( 56.0, COURT_SIZE[0],-20.16,-20.16, zo=1)
+        self.DrawLine(-56.0,-COURT_SIZE[0], 20.16, 20.16, zo=1)
+        self.DrawLine(-56.0,-COURT_SIZE[0],-20.16,-20.16, zo=1)
 
 
-    def DrawLine(self, x1, x2, y1, y2, color="w-"):
-        line = plt.plot([x1,x2],[y1,y2],color,lw=2)
-        #self.ax.add_patch(line)
+    def DrawLine(self, x1, x2, y1, y2, color="w-", zo=3):
+        line = plt.plot([x1,x2],[y1,y2],color,lw=2, zorder=zo)
 
 
     def Show(self):
