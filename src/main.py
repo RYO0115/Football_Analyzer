51ed62148626fd1689d083e9c8b26ad0eb697271
RYO0115 <pastmemories.meiji@gmail.com>
2019-05-03 20:45:57 +0900

    changed home, away team formation

--- src/main.py
+++ src/main.py
@@ -5,5 +5,7 @@ if __name__ == '__main__':
     fig, ax = plt.subplots()
     ps = PLAYER_SERVER(fig, ax)
     ps.SetFullMember()
+    ps.teams[0].SetPlayerPosition("4231")
+    ps.teams[1].SetPlayerPosition("343")
     ps.DrawPlayers()
     ps.Show()
