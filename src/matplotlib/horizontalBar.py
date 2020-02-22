5f9f0ab488101d7efb73c2e7ae70ac02231225a3
RYO0115 <pastmemories.meiji@gmail.com>
2020-02-22 06:31:07 -0700

    比較グラフの機能を追加

--- src/matplotlib/horizontalBar.py
+++ src/matplotlib/horizontalBar.py
@@ -1,10 +1,12 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.patheffects as path_effects
 
 from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
 
 from fileReader import *
+from plt_common import *
 
 import pandas as pd
 from PIL import Image
@@ -42,6 +44,9 @@ gs_master = GridSpec(nrows=GRID_ROWS, ncols=GRID_ROWS)
 
 #Score Board
 gs_scoreboard   = GridSpecFromSubplotSpec(nrows=SCORETABLE_ROWS, ncols=SCORETABLE_COLS, subplot_spec=gs_master[:SCORETABLE_ROW_END, :])
+gs_homeEmblem = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_scoreboard[:, SIDE_MERGIN:HOMETEAMNAME_COL_END])
+gs_awayEmblem = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_scoreboard[:, AWAYTEAMNAME_COL_START:-SIDE_MERGIN])
+
 gs_homeTeamName = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_scoreboard[1:, SIDE_MERGIN:HOMETEAMNAME_COL_END])
 gs_awayTeamName = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_scoreboard[1:, AWAYTEAMNAME_COL_START:-SIDE_MERGIN])
 gs_score        = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_scoreboard[1:, HOMETEAMNAME_COL_END:AWAYTEAMNAME_COL_START])
@@ -49,46 +54,18 @@ gs_date         = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_scor
 
 # Stats Table
 gs_dataname     = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_master[STATS_ROW_START:, HOME_STATS_COL_END:AWAY_STATS_COL_START])
-gs_homelist     = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_master[STATS_ROW_START:,  :HOME_STATS_COL_END])
-gs_awaylist     = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_master[STATS_ROW_START:, AWAY_STATS_COL_START:])
+# No Compare Version
+gs_homelist     = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_master[STATS_ROW_START:,  SIDE_MERGIN:HOME_STATS_COL_END])
+gs_awaylist     = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_master[STATS_ROW_START:, AWAY_STATS_COL_START:-SIDE_MERGIN])
+
+# Comparing Version
+gs_comparelist  = GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=gs_master[STATS_ROW_START:,  SIDE_MERGIN:-SIDE_MERGIN])
+
 
 
 def IsStr(data):
     return(type(data) is str)
 
-def GetFullSizeFigure():
-    fig = plt.figure()
-    fig.subplots_adjust(
-        left=0.0,
-        right=1.0,
-        top=1.0,
-        bottom=0.0
-    )
-    return(fig)
-
-def DeleteSpines(ax):
-    ax.tick_params(bottom=False,
-                left=False,
-                right=False,
-                top=False)
-    ax.spines["right"].set_color("none")  # 右消し
-    ax.spines["left"].set_color("none")   # 左消し
-    ax.spines["top"].set_color("none")    # 上消し
-    ax.spines["bottom"].set_color("none")  # 下消し
-
-def DeleteXTicks(ax):
-    ax.set_xticks(ticks=[])
-
-def DeleteYTicks(ax):
-    ax.set_yticks(ticks=[])
-
-def DeleteTicks(ax):
-    DeleteXTicks(ax)
-    DeleteYTicks(ax)
-
-def DeleteAllOutlines(ax):
-    DeleteSpines(ax)
-    DeleteTicks(ax)
 
 def Rounding(data):
     if data <= 4:
@@ -131,11 +108,12 @@ def GetNormalizedDataList(dataList, maxList):
 
     return(normalizedDataList)
 
-def WriteTextFromCenterOfAxes(ax, str, size=12, color="black"):
-    ax.set_xlim([0,100])
-    ax.set_ylim([0,100])
-    ax.text( 50, 50, str, size=size, ha="center", va="center")
+def GetNormalizedDataList(pdDataList):
+    for i in range(len(pdDataList.index)):
+        for j in range(len(pdDataList.columns)):
+            pdDataList[pdDataList.columns[j]][i] /= pdDataList["Max"][i]
 
+    return(pdDataList)
 
 
 # Draws Horizontal Bar Graph
@@ -151,26 +129,88 @@ def DrawHorizontalBar( ax, data, labels = [], color="r"):
 
     ax.set_yticks(label_pos)
 
-def WriteDataName(ax, dataNameList, color="black", size=6):
+def CreateDominantColorList( homeData, awayData):
+    colorList = [[],[]]
+    if len(homeData) != len(awayData):
+        return(colorList)
+
+    for i in range(len(homeData)):
+        if homeData[i] > awayData[i]:
+            colorList[0].append(0.8)
+            colorList[1].append(0.3)
+        elif homeData[i] < awayData[i]:
+            colorList[0].append(0.3)
+            colorList[1].append(0.8)
+        else:
+            colorList[0].append(0.5)
+            colorList[1].append(0.5)
+
+    return(colorList)
+
+def DrawComparingHorizontalBar( ax, data, leftEnd_data, labels = [], color = [], alpha = []):
+    x_array = np.arange(len(data))
+    bar_list = ax.barh( x_array, data, left=leftEnd_data, edgecolor="White", linewidth=0.8, tick_label=labels, alpha = 0.8)
+
+    for j in range(len(color)):
+        bar_list[j].set_facecolor(color[j])
+
+    for j in range(len(alpha)):
+        bar_list[j].set_alpha(alpha[j])
+
+    leftEnd_data += data
+
+    return(leftEnd_data)
+
+def DrawStatsWithComparingHorizontalBar(ax, pdDataList):
+    homeAway_color = {"Home":"red", "Away":"blue"}
+    #dominant_color = {"Big":"Blue", "small":"glay"}
+
+    labels = pdDataList.index.to_list()
+    leftEnd_data = pd.Series(np.zeros(len(pdDataList.index)), index=labels)
+    for i in range(len(pdDataList.columns)):
+        if pdDataList.columns[i] != "Max":
+            color = [homeAway_color[pdDataList.columns[i]] for j in range(len(pdDataList.index))]
+            alpha = CreateDominantColorList(pdDataList["Home"].values.tolist(), pdDataList["Away"].values.tolist())
+            #leftEnd_data = DrawComparingHorizontalBar(ax, pdDataList[pdDataList.columns[i]], leftEnd_data, labels = labels, color=color)
+            #leftEnd_data = DrawComparingHorizontalBar(ax, pdDataList[pdDataList.columns[i]], leftEnd_data, labels = labels, alpha=alpha[i])
+            leftEnd_data = DrawComparingHorizontalBar(ax, pdDataList[pdDataList.columns[i]], leftEnd_data, labels = labels, color=color, alpha=alpha[i])
+
+
+
+def WriteDataName(ax, dataNameList, color="black", size=6, pos="center"):
     ax.set_xlim([0,100])
     ax.set_ylim([100,0])
     dataNum = len(dataNameList)
     space = 94 / dataNum
     x = 50
+    if pos == "top":
+        y = 0
+    elif pos == "bottom":
+        y = space
+    else:
+        y = space/2
+
     y = space / 2
     for data in dataNameList:
-        ax.text(x, y, data, size=size, ha="center", va="center")
+        text = ax.text(x, y, data, size=size, ha="center", va="center", color=color)
+        text.set_path_effects([path_effects.SimpleLineShadow(), path_effects.Normal()])
         y += space
 
 
 if __name__ == '__main__':
     dataArray = {   "Home": ["Arsenal", 2, 53.2, 10, 4, 2, 701, 87, 48],
-                    "Away": ["Tottenham", 0, 46.8, 6, 3, 2, 350, 81, 58],
-                    "Max":  ["None", 10, 100.0, 20, 20, 20, 1000, 100, 100]}
-
-    data = pd.DataFrame( dataArray, index=[ "TeamName", "Score", "Possesion", "Shots", "Shots on Target", "Created Chance", "Passes", "Completed Passes", "Duels"] )
+                    "Away": ["Tottenham", 0, 46.8, 6, 3, 2, 350, 81, 58]}
+                    #"Max":  ["None", 10, 100.0, 20, 20, 20, 1000, 100, 100]}
+    data = pd.DataFrame( dataArray, index=[ "TeamName", "Score", "Possesion", "Shots", "Shots on Target", "Created Chances", "Passes", "Completed Passes", "Duels"] )
 
     fig = GetFullSizeFigure()
+    ax_backimage = fig.add_subplot(1,1,1)
+    backImageDir = os.path.dirname(os.path.abspath(__file__)) + "/../../image/backimage/"
+    back_im = Image.open(backImageDir + "PremierLeagueTemplate002.jpeg")
+    back_xlim = ax_backimage.get_xlim()
+    back_ylim = ax_backimage.get_ylim()
+    ax_backimage.imshow(back_im, extent=[*back_xlim, *back_ylim], aspect="auto", alpha=0.8)
+    #fig.set_facecolor("lightblue")
     #ax_scoreboard = fig.add_subplot(gs_scoreboard[:,:])
 
     ax_homeTeamName = fig.add_subplot(gs_homeTeamName[:,:])
@@ -179,23 +219,38 @@ if __name__ == '__main__':
     ax_date         = fig.add_subplot(gs_date[:,:])
     DeleteAllOutlines(ax_homeTeamName)
     DeleteAllOutlines(ax_awayTeamName)
+    DeleteAxesColor(ax_homeTeamName)
+    DeleteAxesColor(ax_awayTeamName)
     DeleteAllOutlines(ax_score)
     DeleteAllOutlines(ax_date)
+    DeleteAxesColor(ax_score)
+    DeleteAxesColor(ax_date)
 
     teamNameId  = data.index.get_loc("TeamName")
     scoreId     = data.index.get_loc("Score")
 
-    WriteTextFromCenterOfAxes( ax_homeTeamName, data["Home"].iloc[teamNameId], size=15)
-    WriteTextFromCenterOfAxes( ax_awayTeamName, data["Away"].iloc[teamNameId], size=15)
+    WriteTextFromCenterOfAxes( ax_homeTeamName, data["Home"].iloc[teamNameId], size=15, color="White")
+    WriteTextFromCenterOfAxes( ax_awayTeamName, data["Away"].iloc[teamNameId], size=15, color="White")
 
     scoreStr = str(data["Home"].iloc[scoreId]) + " - " + str(data["Away"].iloc[scoreId])
-    WriteTextFromCenterOfAxes( ax_score, scoreStr, size=24)
+    WriteTextFromCenterOfAxes( ax_score, scoreStr, size=24, color="White")
 
 
     # ------------- Stats Setting ------------
-    ax_dataname = fig.add_subplot(gs_dataname[:,:])
     ax_home = fig.add_subplot(gs_homelist[:,:])
     ax_away = fig.add_subplot(gs_awaylist[:,:])
+    ax_compare = fig.add_subplot(gs_comparelist[:,:])
+    ax_dataname = fig.add_subplot(gs_dataname[:,:])
+    DeleteSpines(ax_compare)
+    DeleteAxesColor(ax_compare)
+    DeleteAxesColor(ax_home)
+    DeleteAxesColor(ax_away)
+    DeleteAxesColor(ax_dataname)
+
+    #DeleteAxesColor()
+    #ax_compare.patch.set_alpha(0.0)
+    #ax_dataname.patch.set_alpha(0.0)
+
 
     ax_home.yaxis.tick_right()
     ax_home.set_xlim([100,0])
@@ -210,21 +265,36 @@ if __name__ == '__main__':
     DeleteSpines(ax_away)
     DeleteAllOutlines(ax_dataname)
 
-    #DrawScoreBoard( ax_scoreboard)
+    homeLabel    = GetDataWithoutNameAndScore(data["Home"]).values.tolist()
+    awayLabel    = GetDataWithoutNameAndScore(data["Away"]).values.tolist()
+
+    data["Max"] = data["Home"] + data["Away"]
+
     homeData    = GetDataWithoutNameAndScore(data["Home"]).values.tolist()
     awayData    = GetDataWithoutNameAndScore(data["Away"]).values.tolist()
-    maxData     = GetDataWithoutNameAndScore(data["Max"]).values.tolist()
+    maxData     = GetDataWithoutNameAndScore(data["Max"])
     indexData   = list(GetDataWithoutNameAndScore(data["Home"]).index)
 
     #normalizedHomeData =
 
-    DrawHorizontalBar(  ax_home, GetNormalizedDataList(homeData, maxData), labels=GetDataNumAsLabel(homeData), color="r")
-    DrawHorizontalBar(  ax_away, GetNormalizedDataList(awayData, maxData), labels=GetDataNumAsLabel(awayData), color="b")
-    WriteDataName( ax_dataname, indexData, size=10)
-    #DrawHorizontalBar(  ax_dataname, [], labels=indexData)
+    #DrawHorizontalBar(  ax_home, GetNormalizedDataList(homeData, maxData), labels=GetDataNumAsLabel(homeData), color="r")
+    #DrawHorizontalBar(  ax_away, GetNormalizedDataList(awayData, maxData), labels=GetDataNumAsLabel(awayData), color="b")
+    #WriteDataName( ax_dataname, indexData, size=10)
+
+    vis_data = data.drop("TeamName")
+    vis_data = GetNormalizedDataList(vis_data)
+    DrawStatsWithComparingHorizontalBar( ax_compare, vis_data)
+    ax_compare.set_ylim([len(vis_data.index),-0.5])
+
+    WriteDataName( ax_dataname, vis_data.index, color="White", size=12, pos="top")
+
+
     DeleteXTicks(ax_home)
     DeleteXTicks(ax_away)
+    DeleteAllOutlines(ax_home)
+    DeleteAllOutlines(ax_away)
     DeleteAllOutlines(ax_dataname)
+    DeleteAllOutlines(ax_compare)
 
     imageDir = os.path.dirname(os.path.abspath(__file__)) + "/../../image/"
     home_im = Image.open(imageDir + "Arsenal_FC.png")
@@ -233,6 +303,7 @@ if __name__ == '__main__':
     home_ylim = ax_home.get_ylim()
     away_xlim = ax_away.get_xlim()
     away_ylim = ax_away.get_ylim()
+
     ax_home.imshow(home_im, extent=[*home_xlim, *home_ylim], aspect="auto", alpha = 0.5)
     ax_away.imshow(away_im, extent=[*away_xlim, *away_ylim], aspect="auto", alpha = 0.5)
 
---
+++ src/matplotlib/plt_common.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
+from PIL import Image
+
+def GetFullSizeFigure():
+    fig = plt.figure()
+    fig.subplots_adjust(
+        left=0.0,
+        right=1.0,
+        top=1.0,
+        bottom=0.0
+    )
+    return(fig)
+
+def DeleteSpines(ax):
+    ax.tick_params(bottom=False,
+                left=False,
+                right=False,
+                top=False)
+    ax.spines["right"].set_color("none")  # 右消し
+    ax.spines["left"].set_color("none")   # 左消し
+    ax.spines["top"].set_color("none")    # 上消し
+    ax.spines["bottom"].set_color("none")  # 下消し
+
+def DeleteXTicks(ax):
+    ax.set_xticks(ticks=[])
+
+def DeleteYTicks(ax):
+    ax.set_yticks(ticks=[])
+
+def DeleteTicks(ax):
+    DeleteXTicks(ax)
+    DeleteYTicks(ax)
+
+def DeleteAllOutlines(ax):
+    DeleteSpines(ax)
+    DeleteTicks(ax)
+
+def DeleteAxesColor(ax):
+    ax.patch.set_alpha(0.0)
+
+def WriteTextFromCenterOfAxes(ax, str, size=12, color="black"):
+    ax.set_xlim([0,100])
+    ax.set_ylim([0,100])
+    ax.text( 50, 50, str, size=size, ha="center", va="center")
